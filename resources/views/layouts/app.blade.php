44122bf71dec648a848a28a7d283594526ce4c16
José Joaquín Garci Peña <josejoaquin.garci@iesdonana.org>
2022-10-30 15:41:40 +0100

    Empezando con Tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,50 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        @livewireStyles
-
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="font-sans antialiased">
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-
-        @stack('modals')
-
         @livewireScripts
+        <nav id="header" class="w-full z-30 top-10 py-1 bg-white shadow-lg border-b border-blue-400 mt-24">
+            <div class="w-full flex items-center justify-between mt-0 px-6 py-2">
+               <label for="menu-toggle" class="cursor-pointer md:hidden block">
+                  <svg class="fill-current text-blue-600" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                     <title>menu</title>
+                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                  </svg>
+               </label>
+               <input class="hidden" type="checkbox" id="menu-toggle">
+
+               <div class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
+                  <nav>
+                     <ul class="md:flex items-center justify-between text-base text-blue-600 pt-4 md:pt-0">
+                        <li><a class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" href="#">Inicio</a></li>
+                        <li><a class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" href="#">Mi perfil</a></li>
+                        <li><a class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" href="#">Reportes</a></li>
+                     </ul>
+                  </nav>
+               </div>
+
+               <div class="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4" id="nav-content">
+                @guest
+                <div class="auth flex items-center w-full md:w-full">
+                    <a class="bg-transparent text-gray-800  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700" href="/login">Iniciar sesión</a>
+                    <a class="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100" href="/register">Regístrate</a>
+                 </div>
+                @endguest
+                @auth
+                <div class="auth flex items-center w-full md:w-full">
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <a class="bg-transparent text-gray-800  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" role="button">Cerrar sesión</a>
+                    </form>
+                 </div>
+                @endauth
+               </div>
+            </div>
+         </nav>
+         <main>
+            {{$slot}}
+         </main>
     </body>
 </html>
