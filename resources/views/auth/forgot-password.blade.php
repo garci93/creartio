eff2e110584579378f1176c7dfdea1a8c9aff7b9
José Joaquín Garci Peña <josejoaquin.garci@iesdonana.org>
2023-01-12 04:25:39 +0100

    Logo en formularios de autenticación

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img class="h-16 mx-auto" src="{{Storage::disk('s3')->url('layout/logo-400-white.png')}}" alt="profile">
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
